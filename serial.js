b978fbd073b4593f5455e999d12670585c443d80
Anand Chowdhary <anandchowdhary@gmail.com>
2018-07-02 15:01:07 +0200

    Start serial communication

--- serial.js
+++ serial.js
@@ -1,7 +1,19 @@
+var WebSocket = require('ws');
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/cu.wchusbserial1410', {
-  baudRate: 9600
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  ws.send('something');
 });
-port.on('readable', function () {
-  console.log('Data:', port.read());
-});
+
+// var value = false;
+// var port = new SerialPort('/dev/cu.wchusbserial1410', {
+//   baudRate: 9600
+// });
+// port.on('data', function (data) {
+//   console.log('Data:', data.toString());
+// });
