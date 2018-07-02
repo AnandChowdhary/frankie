8b2db34f3ff234acc8cc516bade6060be07fc77e
Anand Chowdhary <anandchowdhary@gmail.com>
2018-07-02 11:12:52 +0200

    Add basic TTS/STT

--- src/components/Frankie.vue
+++ src/components/Frankie.vue
@@ -1,32 +1,76 @@
 <template>
   <div>
     <button class="cta">
-      <font-awesome-icon icon="play" />
+      <font-awesome-icon @click="init" v-if="status === 0" icon="play" />
+      <font-awesome-icon @click="startSpeechRecognition" v-else-if="status === 1" icon="microphone" />
+      <font-awesome-icon v-else icon="microphone-slash" />
     </button>
+    <select class="languages" v-model="selectedVoice">
+      <option v-for="(language, id) in languages" v-bind:key="`lang-${id}`" :value="language.name">{{language.name}}</option>
+    </select>
   </div>
 </template>
 
 <script>
-const ACCESS_TOKEN = "1407651166934bd8aecc0defe98d7b7c";
+const ACCESS_TOKEN = '1407651166934bd8aecc0defe98d7b7c';
+const recognition = new webkitSpeechRecognition() || new SpeechRecognition(); // eslint-disable-line
+const synthesis = window.speechSynthesis;
 export default {
   name: 'Frankie',
   data() {
     return {
-      text: ""
+      text: '',
+      status: 0,
+      selectedVoice: null,
+      languages: null
+    }
+  },
+  watch: {
+    languages() {
+      if (!this.selectedVoice) {
+        this.selectedVoice = this.languages[0];
+      }
     }
   },
   methods: {
-    api() {
+    mounted() {
+      this.init();
+    },
+    init() {
+      // Speech recognition
+			recognition.onresult = event => {
+        this.status = 1;
+        const query = event.results[0][0].transcript;
+        this.api(query).then(reply => {
+					try {
+						reply = reply.result.speech;
+					} catch (e) {
+						reply = "I don't know what to say...";
+          }
+          this.speak(reply);
+        });
+      }
+      // Speech synthesis
+      this.languages = synthesis.getVoices();
+      // Start program
+      this.status = 1;
+      // this.startSpeechRecognition();
+    },
+    startSpeechRecognition() {
+      recognition.start();
+      this.status = 2;
+    },
+    api(query) {
       return new Promise((resolve, reject) => {
-        fetch("https://api.dialogflow.com/v1/query", {
-          method: "POST",
+        fetch('https://api.dialogflow.com/v1/query', {
+          method: 'POST',
           headers: {
             Authorization: `Bearer ${ACCESS_TOKEN}`,
-            "content-type": "application/json; charset=utf-8"
+            'content-type': 'application/json; charset=utf-8'
           },
           body: JSON.stringify({
-            query: this.text,
-            lang: "en",
+            query: query,
+            lang: 'en',
             sessionId: Math.random()
               .toString(36)
               .slice(2)
@@ -35,6 +79,19 @@ export default {
           .then(response => response.json())
           .then(json => resolve(json))
           .catch(error => reject(error));
+      });
+    },
+    speak(text) {
+      return new Promise(resolve => {
+        const utterance = new SpeechSynthesisUtterance(text);
+				for (let i = 0; i < this.languages.length; i++) {
+					if (this.languages[i].name == this.selectedVoice) {
+						utterance.voice = this.languages[i];
+						break;
+					}
+				}
+				utterance.onend = () => { resolve(); };
+        speechSynthesis.speak(utterance);
       });
     }
   }
@@ -61,5 +118,10 @@ a {
 .cta:active,
 .cta:focus {
   transform: scale(1.1);
+}
+.languages {
+  position: absolute;
+  left: 1rem;
+  bottom: 1rem;
 }
 </style>
